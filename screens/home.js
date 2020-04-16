9513c5ff5d29a529ba34e23fbe01b1d0577a388d
LeonardvS <schilcherl@gmail.com>
2020-04-16 22:15:32 +0200

    added styling to home screen

--- screens/home.js
+++ screens/home.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, ImageBackground } from 'react-native';
+
+import FlatButton from '../shared/button';
 
 export default function Home ( { navigation }) {
 
@@ -13,15 +15,34 @@ export default function Home ( { navigation }) {
 
   return (
     <View style={styles.container}>
-      <Button title='Drink' color='red' onPress={pressHandlerMaps} />
-      <Button title='Rate' color='coral' onPress={pressHandlerForm} />
+      <ImageBackground source={require('../assets/map.png')} style={styles.top}>
+        <FlatButton style={styles.button} text='drink' onPress={pressHandlerMaps} />
+      </ImageBackground>
+      <View style={styles.line}></View>
+      <ImageBackground source={require('../assets/rate.png')} style={styles.bottom}>
+        <FlatButton style={styles.button} text='rate' onPress={pressHandlerForm} />
+      </ImageBackground>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    padding: 24,
+    padding: 5,
+  },
+  top: {
+    height: Dimensions.get('window').height/2.3,
+    borderRadius: 8,
+    marginBottom: 2
+  },
+  bottom: {
+    height: Dimensions.get('window').height/2.3,
+    borderRadius: 8
+  },
+  line: {
+    borderBottomColor: 'slategrey',
+    borderBottomWidth: 1,
+    marginBottom: 5
   }
 })
 
